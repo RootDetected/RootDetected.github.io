aa8a93f4cb802ec8bcfae8d1b223af0845d310b4
VD $ VD171 @ Priv8 <80125987+VD171@users.noreply.github.com>
2025-07-02 18:05:50 -0300

    Create zygisknext-shamiko-vs-rezygisk-treatwheel-02jul2025.js

---
+++ tables/zygisknext-shamiko-vs-rezygisk-treatwheel-02jul2025.js
@@ -0,0 +1,28 @@
+info="<b>Device: Xiaomi Redmi Note 9</b><br><b>ROM: MIUI V13.0.2.0.SJOMIXM</b><br><b>Android: 12 Kernel: 4.14</b><br><b>Magisk: Alpha 29001</b>";
+
+title="ZygiskNext Shamiko vs ReZygisk TreatWheel - 02/Jul/2025";
+
+columns=["App","Package","Version","ZygiskNext v1.2.9 521-e73dbfc\n + Shamiko v1.2.5-414","ReZygisk 387-b6f02b3\n + Treat Wheel v0.0.3"];
+
+data=[
+    [{text:"BDO Pay",bg:"#fff",color:"#000"},{text:"ph.com.bdo.pay",bg:"#fff",color:"#000"},{text:"1.7.0",bg:"#fff",color:"#000"},{text:"OK",bg:"#006600",color:"#fff"},{text:"OK",bg:"#006600",color:"#fff"}],
+    [{text:"IndianOil For Business",bg:"#fff",color:"#000"},{text:"px.indianoil.in",bg:"#fff",color:"#000"},{text:"3.0.6",bg:"#fff",color:"#000"},{text:"OK",bg:"#006600",color:"#fff"},{text:"OK",bg:"#006600",color:"#fff"}],
+    [{text:"IndianOil For Business",bg:"#fff",color:"#000"},{text:"px.indianoil.in",bg:"#fff",color:"#000"},{text:"3.0.7",bg:"#fff",color:"#000"},{text:"OK",bg:"#006600",color:"#fff"},{text:"OK",bg:"#006600",color:"#fff"}],
+    [{text:"IndianOil ONE",bg:"#fff",color:"#000"},{text:"cx.indianoil.in",bg:"#fff",color:"#000"},{text:"3.1.80",bg:"#fff",color:"#000"},{text:"OK",bg:"#006600",color:"#fff"},{text:"OK",bg:"#006600",color:"#fff"}],
+    [{text:"IndianOil ONE",bg:"#fff",color:"#000"},{text:"cx.indianoil.in",bg:"#fff",color:"#000"},{text:"3.1.81",bg:"#fff",color:"#000"},{text:"OK",bg:"#006600",color:"#fff"},{text:"OK",bg:"#006600",color:"#fff"}],
+    [{text:"Santander UK",bg:"#fff",color:"#000"},{text:"uk.co.santander.santanderUK",bg:"#fff",color:"#000"},{text:"5.21.0",bg:"#fff",color:"#000"},{text:"OK",bg:"#006600",color:"#fff"},{text:"OK",bg:"#006600",color:"#fff"}],
+    [{text:"YONO Nepal SBI",bg:"#fff",color:"#000"},{text:"com.YONONPMobileApp",bg:"#fff",color:"#000"},{text:"1.3.6",bg:"#fff",color:"#000"},{text:"DETECTED",bg:"#ff0000",color:"#fff"},{text:"OK",bg:"#006600",color:"#fff"}],
+    [{text:"YONO Nepal SBI",bg:"#fff",color:"#000"},{text:"com.YONONPMobileApp",bg:"#fff",color:"#000"},{text:"1.3.8",bg:"#fff",color:"#000"},{text:"DETECTED",bg:"#ff0000",color:"#fff"},{text:"OK",bg:"#006600",color:"#fff"}],
+    [{text:"YONO SBI",bg:"#fff",color:"#000"},{text:"com.sbi.lotusintouch",bg:"#fff",color:"#000"},{text:"1.24.21",bg:"#fff",color:"#000"},{text:"DETECTED",bg:"#ff0000",color:"#fff"},{text:"OK",bg:"#006600",color:"#fff"}],
+    [{text:"YONO SBI",bg:"#fff",color:"#000"},{text:"com.sbi.lotusintouch",bg:"#fff",color:"#000"},{text:"1.24.22",bg:"#fff",color:"#000"},{text:"DETECTED",bg:"#ff0000",color:"#fff"},{text:"OK",bg:"#006600",color:"#fff"}],
+    [{text:"YONO SBI",bg:"#fff",color:"#000"},{text:"com.sbi.lotusintouch",bg:"#fff",color:"#000"},{text:"1.24.23",bg:"#fff",color:"#000"},{text:"DETECTED",bg:"#ff0000",color:"#fff"},{text:"OK",bg:"#006600",color:"#fff"}],
+    [{text:"YONO SBI",bg:"#fff",color:"#000"},{text:"com.sbi.lotusintouch",bg:"#fff",color:"#000"},{text:"1.24.24",bg:"#fff",color:"#000"},{text:"DETECTED",bg:"#ff0000",color:"#fff"},{text:"OK",bg:"#006600",color:"#fff"}],
+    [{text:"YONO SBI Maldives",bg:"#fff",color:"#000"},{text:"com.YONOMVMobileApp",bg:"#fff",color:"#000"},{text:"1.4.9",bg:"#fff",color:"#000"},{text:"OK",bg:"#006600",color:"#fff"},{text:"OK",bg:"#006600",color:"#fff"}],
+    [{text:"YONO SBI Maldives",bg:"#fff",color:"#000"},{text:"com.YONOMVMobileApp",bg:"#fff",color:"#000"},{text:"1.5.0",bg:"#fff",color:"#000"},{text:"DETECTED",bg:"#ff0000",color:"#fff"},{text:"OK",bg:"#006600",color:"#fff"}],
+    [{text:"YONO SBI Maldives",bg:"#fff",color:"#000"},{text:"com.YONOMVMobileApp",bg:"#fff",color:"#000"},{text:"1.5.1",bg:"#fff",color:"#000"},{text:"DETECTED",bg:"#ff0000",color:"#fff"},{text:"OK",bg:"#006600",color:"#fff"}],
+    [{text:"Holmes",bg:"#fff",color:"#000"},{text:"me.garfieldhan.holmes",bg:"#fff",color:"#000"},{text:"1.5.1",bg:"#fff",color:"#000"},{text:"Normal",bg:"#006600",color:"#fff"},{text:"Normal",bg:"#006600",color:"#fff"}],
+    [{text:"Hunter",bg:"#fff",color:"#000"},{text:"com.zhenxi.hunter",bg:"#fff",color:"#000"},{text:"6.25",bg:"#fff",color:"#000"},{text:"Normal",bg:"#006600",color:"#fff"},{text:"Normal",bg:"#006600",color:"#fff"}],
+    [{text:"Native Detector",bg:"#fff",color:"#000"},{text:"com.reveny.nativecheck",bg:"#fff",color:"#000"},{text:"7.2.0",bg:"#fff",color:"#000"},{text:"Found injection: Zygisk pid",bg:"#ff0000",color:"#fff"},{text:"Normal",bg:"#006600",color:"#fff"}],
+    [{text:"Native Test",bg:"#fff",color:"#000"},{text:"icu.nullptr.nativetest",bg:"#fff",color:"#000"},{text:"30",bg:"#fff",color:"#000"},{text:"Normal",bg:"#006600",color:"#fff"},{text:"Normal",bg:"#006600",color:"#fff"}],
+    [{text:"Meow Detector",bg:"#fff",color:"#000"},{text:"com.thepedroo.meowdetector",bg:"#fff",color:"#000"},{text:"1.0",bg:"#fff",color:"#000"},{text:"Normal",bg:"#006600",color:"#fff"},{text:"Normal",bg:"#006600",color:"#fff"}],
+];
