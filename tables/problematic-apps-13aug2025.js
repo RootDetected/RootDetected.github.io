2bb6438c58773c14e9113dc054894b49c9ec3eba
VD $ VD171 @ Priv8 <80125987+VD171@users.noreply.github.com>
2025-08-13 17:31:04 -0300

    Create problematic-apps-13aug2025.js

---
+++ tables/problematic-apps-13aug2025.js
@@ -0,0 +1,13 @@
+info="<b>Device: Xiaomi Redmi Note 9</b><br><b>ROM: MIUI V13.0.2.0.SJOMIXM</b><br><b>Android: 12 Kernel: 4.14</b><br><b>Magisk: Alpha 30100</b><br><br><b>RZ: ReZygisk</b><br><b>ZN: ZygiskNext v1.2.9.1-534-b8e7e21</b><br>";
+
+title="Comparison - ReZygisk / ZygiskNext - 04/Aug/2025";
+
+columns=["App","Package","Version","Code","ZN + Shamiko v1.2.5-414","RZ 399-ee6cfc5 + Treat Wheel v0.0.3","RZ 402-7e82331 + Treat Wheel v0.0.3","Locked Bootloader"];
+
+data=[
+	[{text:"CAIXA Tem",bg:"#fff",color:"#000"},{text:"br.gov.caixa.tem",bg:"#fff",color:"#000"},{text:"1.91.16",bg:"#fff",color:"#000"},{text:"2194",bg:"#fff",color:"#000"},{text:"OK",bg:"#006600",color:"#fff"},{text:"OK",bg:"#ff0000",color:"#fff"},{text:"OK",bg:"#ff0000",color:"#fff"},{text:"OK",bg:"#ff0000",color:"#fff"}],
+	[{text:"HDFC Bank",bg:"#fff",color:"#000"},{text:"com.snapwork.hdfc",bg:"#fff",color:"#000"},{text:"11.3.2",bg:"#fff",color:"#000"},{text:"308",bg:"#fff",color:"#000"},{text:"DETECTED",bg:"#006600",color:"#fff"},{text:"DETECTED",bg:"#ff0000",color:"#fff"},{text:"DETECTED",bg:"#ff0000",color:"#fff"},{text:"OK",bg:"#ff0000",color:"#fff"}],
+	[{text:"HDFC Bank App",bg:"#fff",color:"#000"},{text:"com.hdfcbank.android.now",bg:"#fff",color:"#000"},{text:"6.2.7",bg:"#fff",color:"#000"},{text:"56",bg:"#fff",color:"#000"},{text:"DETECTED",bg:"#006600",color:"#fff"},{text:"DETECTED",bg:"#ff0000",color:"#fff"},{text:"DETECTED",bg:"#ff0000",color:"#fff"},{text:"OK",bg:"#ff0000",color:"#fff"}],
+	[{text:"Inter",bg:"#fff",color:"#000"},{text:"br.com.intermedium",bg:"#fff",color:"#000"},{text:"14.0.2",bg:"#fff",color:"#000"},{text:"137550",bg:"#fff",color:"#000"},{text:"OK",bg:"#006600",color:"#fff"},{text:"OK",bg:"#ff0000",color:"#fff"},{text:"OK",bg:"#ff0000",color:"#fff"},{text:"OK",bg:"#ff0000",color:"#fff"}],
+	[{text:"NBK Mobile",bg:"#fff",color:"#000"},{text:"eu.eleader.mobilebanking.nbk",bg:"#fff",color:"#000"},{text:"7.2.19",bg:"#fff",color:"#000"},{text:"281",bg:"#fff",color:"#000"},{text:"OK",bg:"#006600",color:"#fff"},{text:"DETECTED",bg:"#ff0000",color:"#fff"},{text:"DETECTED",bg:"#ff0000",color:"#fff"},{text:"OK",bg:"#ff0000",color:"#fff"}]
+];
