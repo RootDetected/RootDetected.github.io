79f367f0bbc940558adc6a6a8842b0591e06b9d3
VD $ VD171 @ Priv8 <80125987+VD171@users.noreply.github.com>
2025-12-18 16:07:43 -0300

    Create meow-detector-native-18dec2025.js

---
+++ tables/meow-detector-native-18dec2025.js
@@ -0,0 +1,14 @@
+info="<b>Device: Xiaomi Redmi Note 9</b><br><b>ROM: MIUI V13.0.2.0.SJOMIXM</b><br><b>Android: 12 Kernel: 4.14</b><br><b>Magisk: Alpha 30400</b><br>";
+
+title="Comparison - Meow Detector Native - 18/Dec/2025";
+
+columns=["Package","Device #1","Device #2"];
+
+data=[
+	[{text:"Disclosure v1.2.1",bg:"#fff",color:"#000"},{text:"Normal",bg:"#006600",color:"#fff"},{text:"Suspicious file (/system/addon.d) Device spoofing LineageOS in service list LineageOS in /system_ext/etc/selinux/system_ext_sepolicy.cil Uname spoofing (DefaultDispatch) Suspicious memory mapping (LineageOS)",bg:"#ff0000",color:"#fff"}],
+	[{text:"Holmes v1.5.1",bg:"#fff",color:"#000"},{text:"Risky App (1)",bg:"#006600",color:"#fff"},{text:"CRASH before result",bg:"#000",color:"#fff"}],
+	[{text:"Hunter v6.42",bg:"#fff",color:"#000"},{text:"Normal",bg:"#006600",color:"#fff"},{text:"SafetyDetectClient Not Find Security Manager SafetyDetectClient Security Manager Sign Error Find Prop Modify Mark Modified Device Model and ROM",bg:"#ff0000",color:"#fff"}],
+	[{text:"Meow Detector Native v0.0.5",bg:"#fff",color:"#000"},{text:"Evil Services (14)",bg:"#ff0000",color:"#fff"},{text:"Abnormal Surroundings Conventional Tests (12) Suspicious Mounts Unexpected App Custom ROM Detected Abnormal Props",bg:"#ff0000",color:"#fff"}],
+	[{text:"Native Detector v7.6.1",bg:"#fff",color:"#000"},{text:"Abnormal Environment: 611.000000 Mount gap: Mount peer id: 43",bg:"#ff0000",color:"#fff"},{text:"Abnormal Environment: 424.000000 Play Integrity Fix: config_certifiedKeybox",bg:"#006600",color:"#fff"}],
+	[{text:"Native Test v30",bg:"#fff",color:"#000"},{text:"Normal",bg:"#006600",color:"#fff"},{text:"Propery Modified (23) Conventional Tests (10)",bg:"#ff0000",color:"#fff"}]
+];
