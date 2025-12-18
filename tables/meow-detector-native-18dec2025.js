8e46dee67e31cb6447860ea2c10530a25227a28e
VD $ VD171 @ Priv8 <80125987+VD171@users.noreply.github.com>
2025-12-18 16:20:46 -0300

    Update meow-detector-native-18dec2025.js

--- tables/meow-detector-native-18dec2025.js
+++ tables/meow-detector-native-18dec2025.js
@@ -1,8 +1,8 @@
-info="<b>Device: Xiaomi Redmi Note 9</b><br>Kernel: 4.14</b><br><b>ReZygisk v1.0.0-437-3c9e94a</b><br><b>Treat Wheel v0.0.7</b><br>";
+info="<b>Device: Xiaomi Redmi Note 9</b><br><b>Kernel: 4.14</b><br><b>ReZygisk v1.0.0-437-3c9e94a</b><br><b>Treat Wheel v0.0.7</b><br>";
 
 title="Comparison - Meow Detector Native - 18/Dec/2025";
 
-columns=["Package","<b>ROM: MIUI V13.0.2.0.SJOMIXM</b><br><b>Android: 12</b><br><b>Magisk: Alpha 30600</b>","<b>ROM: AxionOS v2.2.1</b><br><b>Android: 16</b><br><b>KernelSU Next (Driver 32334)<br>(Manager v3.0.0-49-gb606f0ab)</b><br><b>SUSFS v2.0.0-CI-208dd1f</b>"];
+columns=["Package","ROM: MIUI V13.0.2.0.SJOMIXM\nAndroid: 12\nMagisk: Alpha 30600","ROM: AxionOS v2.2.1\nAndroid: 16\nKernelSU Next (Driver 32334)\n(Manager v3.0.0-49-gb606f0ab)\nSUSFS v2.0.0-CI-208dd1f"];
 
 data=[
 	[{text:"Disclosure v1.2.1",bg:"#fff",color:"#000"},{text:"Normal",bg:"#006600",color:"#fff"},{text:"Suspicious file (/system/addon.d)<br>Device spoofing<br>LineageOS in service list LineageOS in /system_ext/etc/selinux/system_ext_sepolicy.cil<br>Uname spoofing (DefaultDispatch)<br>Suspicious memory mapping (LineageOS)",bg:"#ff0000",color:"#fff"}],
